51858229678622a6296e857af5e205ecb36fe6fc
atulsangahvi <61201853+atulsangahvi@users.noreply.github.com>
2025-09-21 21:41:13 +0400

    Update and rename tailwind.config.ts to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./*.{ts,tsx,html}"
+  ],
+  theme: { extend: {} },
+  plugins: []
+};
